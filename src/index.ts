2ccbba5f4c384a9308be7e2bac902e3f55542eb7
tmatkovic <toni.matkovic0@gmail.com>
2025-02-28 16:28:23 +0000

    Backend endpoint to generate a checkout session

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { serve } from "@hono/node-server";
 import "dotenv/config";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
@@ -10,8 +11,20 @@ app.get("/", (c) => {
   return c.text("Hello mom!");
 });
 
-app.post("/", (c) => {
-  return c.text("Post request!");
+app.post("/checkout", async (c) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [{ price: "price_1Qx7WqFV8WVarqtCCwXSQwJd", quantity: 1 }],
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    });
+    return c.json(session);
+  } catch (error: any) {
+    console.log(error);
+    throw new HTTPException(500, { message: error?.message });
+  }
 });
 
 serve(
@@ -23,3 +36,11 @@ serve(
     console.log(`Server is running on http://localhost:${info.port}`);
   },
 );
+
+app.get("/success", (c) => {
+  return c.text("Success");
+});
+
+app.get("/cancel", (c) => {
+  return c.text("Cancel");
+});
